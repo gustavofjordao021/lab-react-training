f38db5162c1eb92ac156789bb2f8f0aae2641228
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-03-14 20:14:26 -0400

    Finish iteration 1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import IdCard from './idCard/idCard';
 const App = () => {
   return (
     <div className="App">
-      <h1>IdCard</h1>
         <IdCard
           lastName='Doe'
           firstName='John'
           gender='Male'
-          height={178}
-          birth={new Date("1992-07-14")}
+          height={1.78}
+          birth={new Date("1992","07", "14")}
           picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+        <IdCard
+          lastName='Delores '
+          firstName='Obrien'
+          gender='female'
+          height={1.72}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
         />
       <h1>Greetings</h1>
         {/* TODO: Use the Greetings component */}
--- src/idCard/idCard.js
+++ src/idCard/idCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const IdCard = (props) => {
     return (
-        <div>
+        <div class="id-wrapper">
+                <img src={props.picture} alt="student-pic" id="student-pic"/>
             <div>
-                <img src={props.picture} alt="student-pic"/>
+                <p><strong>First name: </strong>{props.firstName}</p>
+                <p><strong>Last name: </strong>{props.lastName}</p>
+                <p><strong>Gender: </strong>{props.gender}</p>
+                <p><strong>Height: </strong>{props.height}m</p>
+                <p><strong>Birth: </strong>{props.birth.toString()}</p>
             </div>
-            <p><strong>Last name: </strong>{props.lastName}</p>
-            <p><strong>First name: </strong>{props.firstName}</p>
-            <p><strong>Gender: </strong>{props.gender}</p>
-            <p><strong>Height: </strong>{props.height}</p>
-            <p><strong>Birth: </strong>{props.birth}</p>
         </div>
     );
 };
